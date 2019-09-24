0f720531eb9d693ef79183498577e999af9b49f2
Kentaro Hayashi <kenhys@gmail.com>
2019-09-24 11:45:02 +0900

    Implement list command

--- lib/zulip/reminder/cli/commands/list.rb
+++ lib/zulip/reminder/cli/commands/list.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'zulip/reminder/config'
+require 'tty/table'
 require_relative '../command'
 
 module Zulip
@@ -9,11 +11,28 @@ module Zulip
         class List < Zulip::Reminder::Cli::Command
           def initialize(options)
             @options = options
+            @config = Zulip::Reminder::Config.new
           end
 
           def execute(input: $stdin, output: $stdout)
-            # Command logic goes here ...
-            output.puts "OK"
+            path = @config.task_path
+            raw_data = {}
+            if path.exist?
+              raw_data = YAML.load_file(path.to_s)
+            end
+            data = []
+            raw_data.each do |job_id,line|
+              bot = @config.bot.gsub('*', '')
+              user = @config.user.gsub('*', '')
+              task = ""
+              line.match(/echo #{user} (.+)$/) do |matched|
+                task = matched[1]
+              end
+              data << [job_id, task]
+            end
+            table = ::TTY::Table.new ['job id', 'task'], data
+            renderer = TTY::Table::Renderer::Unicode.new(table)
+            puts table.render(:unicode)
           end
         end
       end
